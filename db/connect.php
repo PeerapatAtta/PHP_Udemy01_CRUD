283d8161d179ace6f92f98dd2b2d82a671c9c340
OFF <peerapat13th@gmail.com>
2025-03-17 22:42:53 +0700

    7. User Management is done

--- db/connect.php
+++ db/connect.php
@@ -4,19 +4,20 @@ $host="localhost";
 $username="root";
 $password="";
 $db="employeesdb";
-$dsn="mysql:host=$host;dbname=$db;charset=utf8";
+$dsn="mysql:host=$host;dbname=$db;cahrset=utf8"; // กำหนดค่า DSN สำหรับการเชื่อมต่อกับฐานข้อมูล
 
 try{
-    $pdo = new PDO($dsn,$username,$password);
+    $pdo = new PDO($dsn,$username,$password); // สร้าง object PDO เพื่อเชื่อมต่อกับฐานข้อมูล
 }catch(PDOException $e){
     echo $e->getMessage();
 }
 
-require_once "db/controller.php";
-require_once "db/user.php";
-$controller = new Controller($pdo); // สร้าง object จาก class Controller
-$user = new User($pdo);
+require_once "db/controller.php"; // เรียกใช้งานไฟล์ controller.php
+require_once "db/user.php"; // เรียกใช้งานไฟล์ user.php
 
-$user->insertUser('admin','12345');
+$controller = new Controller($pdo); // สร้าง object จาก class Controller
+$user = new User($pdo); // สร้าง object จาก class User
+
+$user->insertUser('admin','12345'); // เรียกใช้งานฟังก์ชัน insertUser จาก object $user
 
 ?>
--- db/user.php
+++ db/user.php
@@ -1,57 +1,63 @@
-<?php 
-class User{
-    private $db;
-    function __construct($con){
-        $this->db=$con;
+<?php
+class User
+{
+    private $db; // เก็บค่าเชื่อมต่อฐานข้อมูล
+    function __construct($con)
+    { // ฟังก์ชันสร้าง object จาก class User
+        $this->db = $con; // เชื่อมต่อกับฐานข้อมูล
     }
-    function insertUser($username,$password){
-        try{
-            $result=$this->getUserByUserName($username);
-            if($result["num"]>0){
+
+    // ฟังก์ชันสำหรับการเพิ่มข้อมูลผู้ใช้งาน
+    function insertUser($username, $password)
+    {
+        try {
+            $result = $this->getUserByUserName($username); // ทำการเรียกใช้งานฟังก์ชัน getUserByUserName   
+            if ($result["num"] > 0) {
                 return false;
-            }else{
-                $new_password = md5($password.$username);
-                $sql="INSERT INTO users(username,password) VALUES(:username,:password)";
-                $stmt=$this->db->prepare($sql);
-                $stmt->bindParam(":username",$username);
-                $stmt->bindParam(":password",$new_password);
-                $stmt->execute();
+            } else {
+                $new_password = md5($password . $username); // ทำการเข้ารหัสรหัสผ่าน
+                $sql = "INSERT INTO users(username,password) VALUES(:username,:password)"; // คำสั่ง SQL สำหรับการเพิ่มข้อมูล
+                $stmt = $this->db->prepare($sql); // เตรียมคำสั่ง SQL
+                $stmt->bindParam(":username", $username); // กำหนดค่าให้กับพารามิเตอร์ :username
+                $stmt->bindParam(":password", $new_password); // กำหนดค่าให้กับพารามิเตอร์ :password
+                $stmt->execute(); // ทำการเรียกใช้งานคำสั่ง SQL
                 return true;
             }
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return false;
         }
     }
 
-    function getUserByUserName($username){
-        try{
-            $sql="SELECT COUNT(*) as num FROM users WHERE username=:username";
-            $stmt=$this->db->prepare($sql);
-            $stmt->bindParam(":username",$username);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            return $result;
-        }catch(PDOException $e){
+    // ฟังก์ชันสำหรับการตรวจสอบข้อมูลผู้ใช้งาน
+    function getUserByUserName($username)
+    {
+        try {
+            $sql = "SELECT COUNT(*) as num FROM users WHERE username=:username"; // คำสั่ง SQL สำหรับการค้นหาข้อมูล
+            $stmt = $this->db->prepare($sql); // เตรียมคำสั่ง SQL
+            $stmt->bindParam(":username", $username); // กำหนดค่าให้กับพารามิเตอร์ :username
+            $stmt->execute(); // ทำการเรียกใช้งานคำสั่ง SQL
+            $result = $stmt->fetch(); // ดึงข้อมูลออกมา
+            return $result; // ส่งค่ากลับ จำนวนข้อมูลที่พบ
+        } catch (PDOException $e) {
             echo $e->getMessage();
             return false;
         }
     }
 
+    // ฟังก์ชันสำหรับการตรวจสอบข้อมูลผู้ใช้งาน
     function getUser($username,$password){
         try{
-            $sql="SELECT * FROM users WHERE username=:username AND password=:password";
-            $stmt=$this->db->prepare($sql);
-            $stmt->bindParam(":username",$username);
-            $stmt->bindParam(":password",$password);
-            $stmt->execute();
-            $result = $stmt->fetch();
-            return $result;
+            $sql="SELECT * FROM users WHERE username=:username AND password=:password"; // คำสั่ง SQL สำหรับการค้นหาข้อมูล
+            $stmt=$this->db->prepare($sql); // เตรียมคำสั่ง SQL
+            $stmt->bindParam(":username",$username); // กำหนดค่าให้กับพารามิเตอร์ :username
+            $stmt->bindParam(":password",$password); // กำหนดค่าให้กับพารามิเตอร์ :password
+            $stmt->execute(); // ทำการเรียกใช้งานคำสั่ง SQL
+            $result = $stmt->fetch(); // ดึงข้อมูลออกมา
+            return $result; // ส่งค่ากลับ ข้อมูลที่พบ
         }catch(PDOException $e){
             echo $e->getMessage();
             return false;
         }
     }
 }
-
-?>
